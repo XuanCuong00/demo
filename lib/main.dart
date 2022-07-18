787a542f20922ef728828e9d411efff10a7a1e2b
Cuong <xuancuong.dang00@gmail.com>
2022-07-18 10:00:49 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:demo/pages/detail_page.dart';
+import 'package:demo/pages/navpages/main_page.dart';
+import 'package:flutter/material.dart';
+import 'package:demo/pages/welcome_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: DetailPage()
+    );
+  }
+}
+
